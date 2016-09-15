29f2c3077a3056e7b0c0326e0d6b793c98d1f6e8
jackwang <wdmzjjm@163.com>
2016-09-15 12:02:29 +0800

    init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wdmzj
+ * Date: 2016/9/15
+ * Time: 11:52
+ */
+
+namespace App\Http\Controllers;
+
+
+class IndexController extends Controller
+{
+
+    public function showIndex(){
+        return "hello";
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Finderway
+
+ College students' innovative entrepreneurial training program
+
+## License
+
+The Finderway is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
